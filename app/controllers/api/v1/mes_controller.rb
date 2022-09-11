890d428800eda57065d5387f7b6ade2ff7dbb021
dashixiong-11 <2788347834@qq.com>
2022-09-11 21:41:07 +0800

    添加 jwt过期 jwt过期测试

--- app/controllers/api/v1/mes_controller.rb
+++ app/controllers/api/v1/mes_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MesController < ApplicationController
   def show
     user_id = request.env['current_user_id'] rescue nil
-    user = User.find_by_id user_id
+    user = User.find user_id
     return head 404 if user.nil?
     render json: { resource: user }
   end
