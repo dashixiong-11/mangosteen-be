2e0860f314702cf2db4a826b7f16fc49f4587e52
dashixiong-11 <2788347834@qq.com>
2022-09-12 16:54:49 +0800

    删除 测试文件中的header

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -4,7 +4,6 @@ require 'rspec_api_documentation/dsl'
 resource "验证码" do
   post "/api/v1/validation_codes" do
     parameter :email, type: :string
-    header 'Content-Type', 'application/json' 
     let(:email) { '1@qq.com' }
     example "请求发送验证码" do
       expect(UserMailer).to receive(:welcome_email).with(email)
