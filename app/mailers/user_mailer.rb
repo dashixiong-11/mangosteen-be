4445d71906fe6a191e7260214782869f42f7ba75
dashixiong-11 <2788347834@qq.com>
2022-09-25 23:52:46 +0800

    修改发送邮件bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email email
-    validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
+    validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code =  validation_code
     mail(to: email, subject: '山竹记账验证码')
   end
--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "ValidationCodes", type: :request do
   describe "验证码" do
     it "可以被发送" do
-      post '/api/v1/validation_codes', params: {email: 'fangyinghang@foxmail.com'}
+      post '/api/v1/validation_codes', params: {email: '2788347834@qq.com'}
       expect(response).to have_http_status(200)
-      post '/api/v1/validation_codes', params: {email: 'fangyinghang@foxmail.com'}
+      post '/api/v1/validation_codes', params: {email: '2788347834@qq.com'}
       expect(response).to have_http_status(429)
     end
   end
